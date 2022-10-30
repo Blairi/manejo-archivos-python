b14014230fa780c8c746a0cd1f31c2778684ba0d
Blairi <axel.avi.fer@gmail.com>
2022-10-30 13:00:11 -0600

    métodos get y set

--- classes/Todo.py
+++ classes/Todo.py
@@ -11,3 +11,12 @@ class Todo:
     
     def __str__(self) -> str:
         return f"Id: { self.id }\nNombre: { self.nombre }. { self.completado }"
+
+    def get_id(self) -> int|None:
+        return int(self.id)
+
+    def get_nombre(self) -> str|None:
+        return self.nombre
+
+    def is_completado(self) -> bool:
+        return self.completado
